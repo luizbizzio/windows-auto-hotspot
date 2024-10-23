bee17faa7cf8e896df302c36e56edf2f5ee4acf8
Luiz Bizzio <luizrbizzio@gmail.com>
2024-10-23 02:53:19 +0200

    Update windows_auto_hotspot.ps1

--- windows_auto_hotspot.ps1
+++ windows_auto_hotspot.ps1
@@ -1,26 +1,50 @@
-$connectionProfile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetInternetConnectionProfile()
-$tetheringManager = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($connectionProfile)
+function Log-Message {
+    param ($message, $color)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "$timestamp - $message" -ForegroundColor $color
+}
 
 function Await-Operation {
     param ($asyncOperation)
-    while ($asyncOperation.Status -eq "Started") { Start-Sleep -Milliseconds 100 }
+    while ($asyncOperation.Status -eq "Started") {
+        Start-Sleep -Milliseconds 100
+    }
     return $asyncOperation
 }
 
-while ($true) {
-    $ethernet = Get-NetAdapter | Where-Object { $_.MediaType -eq "802.3" -and $_.Status -eq "Up" }
+Log-Message "Starting Windows Auto Hotspot... Let's get your devices connected!" "Cyan"
+Start-Sleep -Seconds 2
 
-    if ($ethernet) {
-        if ($tetheringManager.TetheringOperationalState -eq "Off") {
-            $startOperation = $tetheringManager.StartTetheringAsync()
-            Await-Operation $startOperation
+$checkInterval = 10
+$lastStatus = ""
+
+while ($true) {
+    $ethernetAdapter = Get-NetAdapter | Where-Object { $_.MediaType -eq "802.3" -and $_.Status -eq "Up" }
+    
+    if ($ethernetAdapter) {
+        if ($lastStatus -ne "EthernetConnected") {
+            Log-Message "Ethernet detected! Adapter name: $($ethernetAdapter.Name)" "Green"
+            $lastStatus = "EthernetConnected"
+        }
+
+        $connectionProfile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetInternetConnectionProfile()
+
+        if ($connectionProfile -ne $null) {
+            $tetheringManager = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($connectionProfile)
+
+            if ($tetheringManager.TetheringOperationalState -eq "Off") {
+                Log-Message "Activating Hotspot..." "Yellow"
+                $startOperation = $tetheringManager.StartTetheringAsync()
+                Await-Operation $startOperation
+                Log-Message "Hotspot is now active! Your devices can connect!" "Green"
+            }
         }
     } else {
-        if ($tetheringManager.TetheringOperationalState -eq "On") {
-            $stopOperation = $tetheringManager.StopTetheringAsync()
-            Await-Operation $stopOperation
+        if ($lastStatus -ne "EthernetDisconnected") {
+            Log-Message "No Ethernet connection detected." "Red"
+            $lastStatus = "EthernetDisconnected"
         }
     }
-    
-    Start-Sleep -Seconds 10
+
+    Start-Sleep -Seconds $checkInterval
 }
